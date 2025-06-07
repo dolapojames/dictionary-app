01deac53a0f44a234a5fc15421c8264760e073d1
dolapojames <oludolapo@hotmail.com>
2025-06-07 11:18:48 +0100

    added alt text to Photo component

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,11 @@ export default function Photos(props) {
             return (
               <div className="col-4 mb-2" key={index}>
                 <a href={photo.url} target="blank" rel="noreferrer">
-                  <img src={photo.src.medium} className="img-fluid"></img>
+                  <img
+                    src={photo.src.medium}
+                    className="img-fluid"
+                    alt={photo.alt}
+                  ></img>
                 </a>
               </div>
             );
